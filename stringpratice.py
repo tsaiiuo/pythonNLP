c6502ea189abad4cd263a14d77a5a3de42d23fc1
tsaiiuo <tsaiiuo1@gmail.com>
2021-09-23 15:21:29 +0800

    Create stringpratice.py

---
+++ stringpratice.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 15:18:08 2021
+
+@author: huannn
+"""
+
+a="This"
+b="is"
+c="a Python practice"
+print(c)
+print(c[0])
+print(c[0:5])
+print(c[2:])
+print(c*2)
+print(f"a+b+c={a+' '+b+' '+c}")
