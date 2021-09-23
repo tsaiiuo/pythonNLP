281d222e1798e7ddd0981b9c6ce49a6dc091fa30
tsaiiuo <tsaiiuo1@gmail.com>
2021-09-23 14:59:30 +0800

    Create classpractice.py

---
+++ classpractice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 14:52:14 2021
+
+@author: huannn
+"""
+
+class Lesson:
+    teacher="Ian"
+    def __init__(self,firstname,lastname):
+        self.firstname=firstname
+        self.lastname=lastname
+        
+p=Lesson("John","selly")
+
+print(Lesson.teacher)
+print(p.firstname)
+print(p.lastname)
