894d56d97869539ae2bf73610c913df23fa1b22d
tsaiiuo <tsaiiuo1@gmail.com>
2021-09-23 14:59:21 +0800

    Create practice0204.py

---
+++ practice0204.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 13:42:40 2021
+
+@author: huannn
+"""
+a=int(input("Please input first number:"))
+b=int(input("Please input second number:"))
+c=int(input("Please input third number:"))
+print(f"answer: {max(a,b,c)}")
